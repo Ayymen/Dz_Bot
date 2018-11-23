c2ded9e33c732e7b5ecdd8ccc59bfa04ec456139
aymentlm <45010731+aymentlm@users.noreply.github.com>
2018-11-23 13:23:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,8 +31,17 @@ const sql = require("sqlite");
 ,ti={}  
 ,spee={};
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Dz_BOT | !help`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
  
-
 const superagent = require("superagent")
 client.on('message' , async (message) => {
  if (message.content.startsWith(prefix + 'yn')) {
